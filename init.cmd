c7a85f8a64da3f5ece2a59ec6bdb8181b1961268
unknown <you@example.com>
2014-02-21 22:34:40 +0100

    cc

--- init.cmd
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/alphasonic/test2
-git remote add origin https://alphasonic:landscape86@github.com/alphasonic/test2.git
--- install.bat
+++
@@ -1,4 +0,0 @@
-git init
-git remote add origin https://alphasonic:landscape86@github.com/alphasonic/test2.git
-git pull origin master
-pause
